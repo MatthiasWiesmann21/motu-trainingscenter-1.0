900a89d4c3ac6bfa4c5dc25ba01929a36d13f008
MatthiasWiesmann <matthiaswiesmann21@outlook.com>
2024-11-19 15:16:54 +0100

    trying to solve the issue

--- app/api/auth/container/route.ts
+++ app/api/auth/container/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const url = new URL(request.url)?.origin.includes("localhost")
-    ? "https://cludev1.clubyte.live"
+    ? "https://motu.clubyte.live"
     : new URL(request.url)?.origin;
 
   const container = await db.container.findFirst({
